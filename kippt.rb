5c69b9610482d398badf456cf3c8f49344cc185f
Ajmal Afif <ajmalafif@gmail.com>
2013-08-07 06:47:35 +0800

    added git ci -musr/bin/ruby to file

--- kippt.rb
+++ kippt.rb
@@ -1,3 +1,12 @@
+#!/usr/bin/ruby
+
 require 'kippt'
 
 client = Kippt::Client.new(username: "ajmalafif", token: "ec4d9e2988a179cc5342c635f6a6404139367d43")
+
+account = client.account
+account.username #=> "vesan"
+account = client.account(true) # includes the API token
+account.api_token    #=> "2544d6bfddf5893ec8617"
+
+puts "whats up #{account}"
