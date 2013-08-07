28bb54f62840a077908eb85ce43ac6fbd5439fb5
Ajmal Afif <ajmalafif@gmail.com>
2013-08-07 08:31:44 +0800

    use q: to get queries

--- kippt.rb
+++ kippt.rb
@@ -4,18 +4,18 @@ require 'kippt'
 
 client = Kippt::Client.new(username: "ajmalafif", token: "ec4d9e2988a179cc5342c635f6a6404139367d43")
 
-account = client.account
-account.username #=> "vesan"
-account = client.account(true) # includes the API token
-account.api_token    #=> "2544d6bfddf5893ec8617"
+# account = client.account
+# account.username #=> "vesan"
+# account = client.account(true) # includes the API token
+# account.api_token    #=> "2544d6bfddf5893ec8617"
 
 puts "Search kippt: "
-kippt = gets.chomp
-search = client.clips.search("kippt")
+kippt = gets
+search = client.clips.search(q: "kippt")
 
 # clips = client.clips.all(limit: 25, offset: 50)
 
-print "#{search}"
+# puts "#{search}"
 
 # right now stuck on how to print out lists or clips instead
 # of having an output printed like this right now 
